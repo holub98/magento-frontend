92b9fa48ebeed1c152b9c437147f9e793649bcad
Gabriel <brysiekholub@gmail.com>
2023-07-01 20:59:39 +0200

    fix(frontend): change file structure and fix nav dispay

--- src/view/SecondSection/HamburgerButton/hamburger.js
+++ src/view/SecondSection/HamburgerButton/hamburger.js
@@ -1,7 +1,8 @@
-document.write(`<button class="hamburger" id="button-hamburger">
-          <img
-            src="../public/icons/hamburger.png"
-            alt="hamburger"
-            class="image-hamburger" />
-        </button>
-        `);
+document.write(`
+<button class="hamburger" id="button-hamburger">
+  <img
+    src="../public/icons/hamburger.png"
+    alt="hamburger"
+    class="image-hamburger"
+  />
+</button>`);
--- src/view/SecondSection/Search/search.js
+++ src/view/SecondSection/Search/search.js
@@ -1,12 +1,7 @@
-document.write(`<div class="searching" id="search">
-<form class="input-form">
-          <input
-            type="text"
-            placeholder="Co chcesz kupić?"
-            class="field-search" />
-            </form>
-          <img
-            class="field-image"
-            src="../public/icons/looking.png"
-            alt="magnifier" />
-        </div>`);
+document.write(`
+<div class="searching" id="search">
+  <form class="input-form">
+    <input type="text" placeholder="Co chcesz kupić?" class="field-search" />
+  </form>
+  <img class="field-image" src="../public/icons/looking.png" alt="magnifier" />
+</div>`);
